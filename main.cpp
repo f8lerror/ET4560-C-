32484a870d5dec306946c8b7ea28d6f49871ea46
f8lerror <f8lerror@outlook.com>
2016-03-21 18:38:56 -0600

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,178 @@
+/*
+ * Joshua Wilson
+ * 14 March 2016
+ * Unit 1 Lab 2
+ */
+
+#include <iostream>
+#include <sys/unistd.h>
+
+using namespace std;
+
+/*int main ()
+{
+    int eggs = 12;                                                          // How many eggs per carton
+    cin >> eggs;
+    int day = 24;                                                           // How many hours in a single day
+    int cartons;                                                            // How many cartons will be collected in a single day
+    cartons = eggs * 10 / day;                                              // 60 eggs collected in 12 hours (5 cartons packed)
+    cout << "The farmer packed " << cartons << " cartons per day.\n\n" << endl;        // How many cartons he packed in 12 hours of work
+    cout << "The farmer is old as shit." << endl;
+    cin.get();                                                              // int for how many eggs per carton and how many cartons packed total
+
+    return 0;
+}
+*/
+
+/*
+int main()
+{
+    int freeze = 32, boil = 212; freeze = 0; boil = 100;
+    cout << freeze << endl << boil << endl;
+    return 0;
+}
+*/
+
+/*
+int main ()
+{
+    int x = 0, y = 2;
+    x = y * 4;
+    cout << x << endl << y << endl;
+    return 0;
+
+}
+ */
+
+/*
+int main ()
+{
+    cout << "I am the incredible";
+    cout << "computing \nmachine";
+    cout << "\nand I will \namaze\n";
+    cout << "you \n";
+    return 0;
+}
+*/
+
+/*
+int main ()
+{
+    cout << "Be careful!\n";
+    cout << "This might /n be a trick ";
+    cout << "question.\n";
+    return 0;
+}*/
+
+/*
+int main ()
+{
+    int a, x = 23;
+    a = x % 2;
+    cout << x << endl << a << endl;
+    return 0;
+}*/
+
+/*
+int main ();
+}
+int a, b, c   // Three integers
+a = 3
+b = 4
+c = a + b
+Cout < "The value of c is %d" < C;
+return 0;
+{
+*/
+
+/*
+int main ()
+{
+    int a = 3;
+    int b = 4;
+    int c;
+    c = a + b;
+    cout << "The value of c is " << c << endl;
+}*/
+
+/*
+
+int main ()
+{
+    double totsal;                                                                  // totsal is the number of total sales
+    double ecd;                                                                     // ecd is the East Coast Division
+    double perc;                                                                    // perc is the percentage
+    string p;
+    p = "F8LERROR";
+
+    cout << "What are the total sales for the Ludicrous Company INC.? "<< endl;     // ask how much the total sales are
+    cin >> totsal;                                                                  // input the company's total sales for the year
+    cout << "Ludicrous Company INC. total sales are: " << totsal << endl;           // display the total sales entered by the user
+    perc = 62;                                                                      // static percentage the East Coast Division is not responsible for
+
+    ecd = (perc / 100) * totsal;                                                    // math so complicated had to hire a 5 year old japanese kid to calculate it
+
+    cout << "East Coast division sales of total sales (62%): " << ecd << endl;      // print the percentage of total sales the East Coast division is responsible for
+    cout << "\n";
+    cout << "Yes I know it is in E notation, WAH math is hard!!!" << p << endl;
+    cin.get ();
+    return 0;
+}*/
+
+
+/*int main ()
+{
+    int acre;
+    double sqm;
+    double sqft;
+    double parcel;
+
+    acre = 43560;                                                                                                                   // 1 acre is equal to 43,560 SqFt
+    parcel = 4;
+
+    cout << "1 acre is equal to 43560 SqFt. How many SqFt and SqMt are in 1/4 of an acre?" << endl;
+    cout << "Now lets do some calculations to find out how many Square Feet and Square Meters are in the parcel\n" << endl;
+    cout << "Processing please wait, converting to metric is annoying even for a computer!\n" << endl;
+
+    sleep (2);
+    cout << ".....-->" << endl;
+    sleep (2);
+    cout << "..........-->" << endl;
+    sleep (2);
+    cout << "...............-->" << endl;
+    sleep (2);
+    cout << "Found little asian kid for calculations instead, this should go much faster now!" << endl;
+    sleep (2);
+    cout << "Finished, here is your answer" << endl;
+
+    sqft = acre / parcel;
+    sqm = sqft / 10.764;
+
+    cout << "The total SqFt of the parcel are: " << sqft << endl;
+    cout << "The total SqMt of the parcel are: " << sqm << endl;
+
+    return 0;
+}*/
+
+/*
+int main ()
+{
+    string name;
+    string address;
+    string phone;
+    string major;
+
+    name = "                Joshua Wilson";
+    address = "             9740 S. Redwood Rd\n                              South Jordan, UT 84095";
+    phone = "        (512) 949-9043";
+    major = "       EECT (Electronics Engineering & Communications Technology";
+
+    cout << "Hi there. Here is all my information for someone to use as they please \n" << endl;
+    cout << "Processing and displaying information received, please wait, this should only take 3 seconds" << endl;
+    sleep (5);
+    cout << "Your name is: " << name << "\n" << "Your Address is: " << address << "\n" << "Your phone number is: " << phone << "\n" << "Your current major is: " << major << "\n" << endl;
+    cout << "Otay that actually took 5 seconds, sue me!" << endl;
+
+    cin.get ();
+    return 0;
+}*/
